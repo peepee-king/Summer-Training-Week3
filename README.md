db2d6c8cb44d9c27691ff7952e04e8860e47adb3
Shownzzz <qq14525457@gmail.com>
2025-07-09 15:35:03 +0800

    Training-ver.0709

--- README.md
+++ README.md
@@ -1,1 +1,71 @@
-# Summer-Training-Week3
+# Week 3 — Detectron2 安裝與降版
+
+## 學習目標
+
+* 學習如何從 GitHub 下載並使用稍舊版 Detectron2 (v0.5)。
+* 透過使用 Conda 建立虛擬環境，處理 PyTorch 版本與相容性問題。
+* 具備進行模型訓練（training）與推論（inference）的能力。
+* 學會修改框架設定檔（config files）以進行客製化調整。
+* 學會如何在Detectron2上註冊資料集
+
+## 作業內容
+
+### 1. 環境安裝與配置
+
+* 建立 Conda 虛擬環境。
+* 安裝並設定 PyTorch 版本。
+* 從 GitHub clone Detectron2。
+* 安裝所需相關依賴套件(detectron2==0.5)並確認環境運作正常。
+```!bash
+pip install git+https://github.com/facebookresearch/detectron2.git@v0.5
+```
+
+### 2. 模型訓練與推論
+
+* 註冊與使用Cityscapes、Foggy_Cityscapes資料集訓練模型
+* 執行train_net.py，確保訓練流程正常運行，並儲存訓練結果。
+* 撰寫 inference 腳本，利用訓練好的模型對資料集(僅測試集)內圖片進行物件偵測。
+
+### 3. 修改並客製化設定檔
+
+* 學習設定檔結構，調整訓練參數，如iteration數量、batch size等。
+* 驗證修改後的設定檔是否成功作用於模型訓練與推論。
+
+## 繳交內容
+
+* 完整的環境安裝步驟說明。
+* 模型訓練與推論結果截圖。
+* 遇到的問題與如何解決。
+* 修改後的 config 檔案，並說明修改的目的與成果。
+
+請先Fork作業repository，到[Github作業](https://github.com/ccuvislab/Summer-Training-Week3)點選Fork到自己的github帳號內，後續clone,commit,push等都在自己的repository內進行。
+* 除為了Foggy資料集修改的config檔案與inference檔案push上去外，說明與結果請改寫成一份新的README.md檔案
+ 
+
+
+## 注意事項
+
+1. 目前已經有train_net.py與Cityscapes的config檔(使用resnet50作為backbone)，請先試著使用Cityscapes資料集完成模型訓練(不需要修改config)，後續再使用Foggy資料集並修改一些config參數完成訓練
+```bash
+ python train_net.py --config-file ./configs/city_resnet50.yaml --num-gpus 1 SOLVER.IMS_PER_BATCH 2 SOLVER.BASE_LR 0.0025
+```
+2. 針對環境安裝問題每個人大不相同，以我測試RTX 4060 Ti為例，這顆GPU似乎不支援pytorch 2.1.0以下版本，建立環境時請選擇適當的python版本: 
+我的環境為python(3.8.20),pytorch(2.1.0),CUDA 11.8
+先使用conda建立好環境，之後"請在環境內安裝套件"以便做好環境控管
+```!bash
+conda create -n myenv python=3.8.20
+conda activate myenv
+```
+可以從[Pytorch網站](https://pytorch.org/get-started/previous-versions/)找到想要的安裝環境指令(依上述我想要的環境設定)
+在選擇版本時並不限於哪種版本，"只要支援且能跑就好"
+```!bash
+conda install pytorch==2.1.0 torchvision==0.16.0 torchaudio==2.1.0 pytorch-cuda=11.8 -c pytorch -c nvidia
+```
+另外我測試環境時同樣也有些小問題需要處理，像是安裝pytorch(2.1.0)時會順便安裝Pillow(10.4.0)，但在舊版detectron2中並不支援太新的Pillow套件，因此需要額外做降版
+```
+pip uninstall pillow
+pip install pillow==9.0.1
+```
+3. 下載detectron2套件前同樣需要先準備好環境(gcc)，否則無法順利安裝在環境上，詳細說明可以[detectron2安裝說明](https://github.com/facebookresearch/detectron2/blob/main/INSTALL.md)查看
+4. 資料集可以從NAS上的VIS_Lab/VIS_Lab共用(ALL) /99_Others/dataset/MSDAOD_dataset中找到，另外註冊資料集時也請確保路徑是正確的
+5. 使用TWCC時可參考的[映象檔資訊](https://man.twcc.ai/@twccdocs/ccs-concept-image-main-zh/%2F%40twccdocs%2Fccs-concept-image-pytorch-zh)
---
+++ train_net.py
@@ -0,0 +1,271 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright (c) Facebook, Inc. and its affiliates.
+"""
+A main training script.
+
+This scripts reads a given config file and runs the training or evaluation.
+It is an entry point that is made to train standard models in detectron2.
+
+In order to let one script support training of many models,
+this script contains logic that are specific to these built-in models and therefore
+may not be suitable for your own project.
+For example, your research project perhaps only needs a single "evaluator".
+
+Therefore, we recommend you to use detectron2 as an library and take
+this file as an example of how to use the library.
+You may want to write your own script with your datasets and other customizations.
+"""
+
+# +
+import numpy as np
+import os
+import xml.etree.ElementTree as ET
+from typing import List, Tuple, Union
+
+from detectron2.data import DatasetCatalog, MetadataCatalog
+from detectron2.structures import BoxMode
+from detectron2.utils.file_io import PathManager
+
+__all__ = ["load_voc_instances", "register_pascal_voc"]
+
+# fmt: off
+CLASS_NAMES = (
+    "aeroplane", "bicycle", "bird", "boat", "bottle", "bus", "car", "cat",
+    "chair", "cow", "diningtable", "dog", "horse", "motorbike", "person",
+    "pottedplant", "sheep", "sofa", "train", "tvmonitor"
+)
+# fmt: on
+
+def load_voc_instances(dirname: str, split: str, class_names: Union[List[str], Tuple[str, ...]]):
+    """
+    Load Pascal VOC detection annotations to Detectron2 format.
+
+    Args:
+        dirname: Contain "Annotations", "ImageSets", "JPEGImages"
+        split (str): one of "train", "test", "val", "trainval"
+        class_names: list or tuple of class names
+    """
+    with PathManager.open(os.path.join(dirname, "ImageSets", "Main", split + ".txt")) as f:
+        fileids = np.loadtxt(f, dtype=str)
+
+    # Needs to read many small annotation files. Makes sense at local
+    annotation_dirname = PathManager.get_local_path(os.path.join(dirname, "Annotations/"))
+    dicts = []
+    for fileid in fileids:
+        anno_file = os.path.join(annotation_dirname, fileid + ".xml")
+        jpeg_file = os.path.join(dirname, "JPEGImages", fileid + ".jpg") 
+        if not os.path.exists(jpeg_file):
+            jpeg_file = os.path.join(dirname, "JPEGImages", fileid + ".png")    #讀不到jpg檔案就讀png檔案
+
+        with PathManager.open(anno_file) as f:
+            tree = ET.parse(f)
+
+        r = {
+            "file_name": jpeg_file,
+            "image_id": fileid,
+            "height": int(tree.findall("./size/height")[0].text),
+            "width": int(tree.findall("./size/width")[0].text),
+        }
+        instances = []
+
+        for obj in tree.findall("object"):
+            cls = obj.find("name").text
+            if cls not in class_names: # 如果此物件的類別不在我們設定的 class_names 裡，就直接跳過
+                continue
+            # We include "difficult" samples in training.
+            # Based on limited experiments, they don't hurt accuracy.
+            # difficult = int(obj.find("difficult").text)
+            # if difficult == 1:
+            # continue
+            bbox = obj.find("bndbox")
+            bbox = [float(bbox.find(x).text) for x in ["xmin", "ymin", "xmax", "ymax"]]
+            # Original annotations are integers in the range [1, W or H]
+            # Assuming they mean 1-based pixel indices (inclusive),
+            # a box with annotation (xmin=1, xmax=W) covers the whole image.
+            # In coordinate space this is represented by (xmin=0, xmax=W)
+            bbox[0] -= 1.0
+            bbox[1] -= 1.0
+            instances.append(
+                {"category_id": class_names.index(cls), "bbox": bbox, "bbox_mode": BoxMode.XYXY_ABS}
+            )
+        r["annotations"] = instances
+        dicts.append(r)
+    return dicts
+
+def register_pascal_voc(name, dirname, split, year, class_names):
+    DatasetCatalog.register(name, lambda: load_voc_instances(dirname, split, class_names))
+    MetadataCatalog.get(name).set(
+        thing_classes=list(class_names), dirname=dirname, year=year, split=split, evaluator_type="coco"
+    )
+
+
+# -
+
+import logging
+import os
+from collections import OrderedDict
+
+# +
+#from DLCV_ex1.vgg import build_vgg_backbone
+#from DLCV_ex1.config import add_vgg_config
+# -
+
+import detectron2.utils.comm as comm
+from detectron2.checkpoint import DetectionCheckpointer
+from detectron2.config import get_cfg
+from detectron2.data import MetadataCatalog
+from detectron2.engine import DefaultTrainer, default_argument_parser, default_setup, hooks, launch
+from detectron2.evaluation import (
+    CityscapesInstanceEvaluator,
+    CityscapesSemSegEvaluator,
+    COCOEvaluator,
+    COCOPanopticEvaluator,
+    DatasetEvaluators,
+    LVISEvaluator,
+    PascalVOCDetectionEvaluator,
+    SemSegEvaluator,
+    verify_results,
+)
+from detectron2.modeling import GeneralizedRCNNWithTTA
+
+
+def build_evaluator(cfg, dataset_name, output_folder=None):
+    """
+    Create evaluator(s) for a given dataset.
+    This uses the special metadata "evaluator_type" associated with each builtin dataset.
+    For your own dataset, you can simply create an evaluator manually in your
+    script and do not have to worry about the hacky if-else logic here.
+    """
+    if output_folder is None:
+        output_folder = os.path.join(cfg.OUTPUT_DIR, "inference")
+    evaluator_list = []
+    evaluator_type = MetadataCatalog.get(dataset_name).evaluator_type
+    if evaluator_type in ["sem_seg", "coco_panoptic_seg"]:
+        evaluator_list.append(
+            SemSegEvaluator(
+                dataset_name,
+                distributed=True,
+                output_dir=output_folder,
+            )
+        )
+    if evaluator_type in ["coco", "coco_panoptic_seg"]:
+        evaluator_list.append(COCOEvaluator(dataset_name, output_dir=output_folder))
+    if evaluator_type == "coco_panoptic_seg":
+        evaluator_list.append(COCOPanopticEvaluator(dataset_name, output_folder))
+    if evaluator_type == "cityscapes_instance":
+        return CityscapesInstanceEvaluator(dataset_name)
+    if evaluator_type == "cityscapes_sem_seg":
+        return CityscapesSemSegEvaluator(dataset_name)
+    elif evaluator_type == "pascal_voc":
+        return PascalVOCDetectionEvaluator(dataset_name)
+    elif evaluator_type == "lvis":
+        return LVISEvaluator(dataset_name, output_dir=output_folder)
+    if len(evaluator_list) == 0:
+        raise NotImplementedError(
+            "no Evaluator for the dataset {} with the type {}".format(dataset_name, evaluator_type)
+        )
+    elif len(evaluator_list) == 1:
+        return evaluator_list[0]
+    return DatasetEvaluators(evaluator_list)
+
+
+class Trainer(DefaultTrainer):
+    """
+    We use the "DefaultTrainer" which contains pre-defined default logic for
+    standard training workflow. They may not work for you, especially if you
+    are working on a new research project. In that case you can write your
+    own training loop. You can use "tools/plain_train_net.py" as an example.
+    """
+
+    @classmethod
+    def build_evaluator(cls, cfg, dataset_name, output_folder=None):
+        return build_evaluator(cfg, dataset_name, output_folder)
+    
+    def run_step(self):
+        super().run_step()  # 保留原來的訓練步驟
+
+        # 記錄學習率和損失
+        lr = self.optimizer.param_groups[0]["lr"]  # 獲取學習率
+        total_loss = self.storage.history("total_loss").avg(20)  # 取最近20次的平均 total loss
+
+    @classmethod
+    def test_with_TTA(cls, cfg, model):
+        logger = logging.getLogger("detectron2.trainer")
+        # In the end of training, run an evaluation with TTA
+        # Only support some R-CNN models.
+        logger.info("Running inference with test-time augmentation ...")
+        model = GeneralizedRCNNWithTTA(cfg, model)
+        evaluators = [
+            cls.build_evaluator(
+                cfg, name, output_folder=os.path.join(cfg.OUTPUT_DIR, "inference_TTA")
+            )
+            for name in cfg.DATASETS.TEST
+        ]
+        res = cls.test(cfg, model, evaluators)
+        res = OrderedDict({k + "_TTA": v for k, v in res.items()})
+        return res
+
+
+def setup(args):
+    """
+    Create configs and perform basic setups.
+    """
+    cfg = get_cfg()
+    cfg.merge_from_file(args.config_file)
+    cfg.merge_from_list(args.opts)
+    cfg.freeze()
+    default_setup(cfg, args)
+    return cfg
+
+
+def main(args):
+    cfg = setup(args)
+    if args.eval_only:
+        model = Trainer.build_model(cfg)
+        DetectionCheckpointer(model, save_dir=cfg.OUTPUT_DIR).resume_or_load(
+            cfg.MODEL.WEIGHTS, resume=args.resume
+        )
+        res = Trainer.test(cfg, model)
+        if cfg.TEST.AUG.ENABLED:
+            res.update(Trainer.test_with_TTA(cfg, model))
+        if comm.is_main_process():
+            verify_results(cfg, res)
+        return res
+
+    """
+    If you'd like to do anything fancier than the standard training logic,
+    consider writing your own training loop (see plain_train_net.py) or
+    subclassing the trainer.
+    """
+    trainer = Trainer(cfg)
+    trainer.resume_or_load(resume=args.resume)
+    if cfg.TEST.AUG.ENABLED:
+        trainer.register_hooks(
+            [hooks.EvalHook(0, lambda: trainer.test_with_TTA(cfg, trainer.model))]
+        )
+    return trainer.train()
+
+
+def invoke_main() -> None:
+    args = default_argument_parser().parse_args()
+    print("Command Line Args:", args)
+    launch(
+        main,
+        args.num_gpus,
+        num_machines=args.num_machines,
+        machine_rank=args.machine_rank,
+        dist_url=args.dist_url,
+        args=(args,),
+    )
+
+
+# +
+dataset_path = 'C:/Users/user/Probabilistic Teacher/ProbabilisticTeacher/data/VOC2007_citytrain' # 這是你的資料集路徑
+class_names = ('truck','car','rider','person','train','motorcycle','bicycle','bus')
+
+
+if __name__ == "__main__":
+    register_pascal_voc("cityscapes_train",dataset_path,"trainval","2007",class_names)
+    register_pascal_voc("cityscapes_val",dataset_path,"test","2007",class_names)
+    invoke_main()  # pragma: no cover
